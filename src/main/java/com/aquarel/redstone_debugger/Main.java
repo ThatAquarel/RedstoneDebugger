85fc638cec032ed91923f400f0fe97950dfd2268
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-11-14 21:09:05 -0500

    Remove Fabric API

--- src/main/java/com/aquarel/redstone_debugger/Main.java
+++ src/main/java/com/aquarel/redstone_debugger/Main.java
@@ -2,23 +2,23 @@ package com.aquarel.redstone_debugger;
 
 import com.aquarel.redstone_debugger.block.Breakpoint;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
     //    public static final Logger LOGGER = LogManager.getLogger("modid");
-    public static final Breakpoint BREAKPOINT = new Breakpoint(FabricBlockSettings.of(Material.METAL).strength(0.1f));
+    public static final Breakpoint BREAKPOINT = new Breakpoint(AbstractBlock.Settings.of(Material.METAL).strength(0.1f));
 
     @Override
     public void onInitialize() {
         Identifier identifier = new Identifier("redstone_debugger", "breakpoint");
 
         Registry.register(Registry.BLOCK, identifier, BREAKPOINT);
-        Registry.register(Registry.ITEM, identifier, new BlockItem(BREAKPOINT, new FabricItemSettings().group(ItemGroup.REDSTONE)));
+        Registry.register(Registry.ITEM, identifier, new BlockItem(BREAKPOINT, new Item.Settings().group(ItemGroup.REDSTONE)));
     }
 }
